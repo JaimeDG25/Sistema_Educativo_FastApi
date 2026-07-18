0c2e9b1efddf708c3d8b1554610bbad55d1c99fe
jhair garcia <garciajhair22@gmail.com>
2026-07-18 15:21:53 -0500

    e

--- app/main.py
+++ app/main.py
@@ -37,6 +37,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://async-courses-.*-kalcios-projects\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
